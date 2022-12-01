6b3ebb09f382cbeb5829bf9998555e988778fd25
Diliara Ibrahim <dilya14@mail.ru>
2022-12-01 09:10:45 +0300

    Digital Clock

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-let index=0;
-show_image(index);
+window.addEventListener('load',calculateTime);
+function calculateTime(){
+        let date=new Date();
+        var dayNumber=date.getDay();
+        let hour=date.getHours();
+        let minute=date.getMinutes();
+        let month=date.getMonth();
+        let ampm=hour>=12?'PM':'AM';
+        let dayNames=["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+        let monthNames=["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+        let dayNumber2=date.getDate();
+        let year=date.getFullYear();
 
-function show_image(i){
-        index+=i;
-        console.log(index);
+        hour=hour%12;
+        
+        hour=hour?hour:'12';
+        hour=hour<10?'0'+hour:hour;
+        minute=minute<10?'0'+minute:minute;
 
-        let images=document.getElementsByClassName("image");
+        document.getElementById("day").innerHTML=dayNames[dayNumber];
+        document.getElementById("hour").innerHTML=hour;
+        document.getElementById("minute").innerHTML=minute;
+        document.getElementById("ampm").innerHTML=ampm;
+        document.getElementById("month").innerHTML=monthNames[month];
+        document.getElementById("date2").innerHTML=dayNumber2;
+        document.getElementById("year").innerHTML=year;
 
-        let dots=document.getElementsByClassName("dot");
-
-        for(i=0;i<images.length;i++){
-                images[i].style.display="none";
-        }
-        for(i=0;i<dots.length;i++){
-                dots[i].className=dots[i].className.replace(" active","");
-        }
-        if(index>images.length-1){
-                index=0;
-        }
-        if(index<0){
-                index=images.length-1;
-        }
-        images[index].style.display="block";
-        dots[index].className+=" active";
-
+        setTimeout(calculateTime,200);
 }
